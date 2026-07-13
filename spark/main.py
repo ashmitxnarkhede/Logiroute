e68a07e8a306722015e46c70a70f40aba537604c
ashmitxnarkhede <ashmitxnarkhede10@gmail.com>
2026-07-13 16:36:40 +0530

    Add Kafka to Spark streaming pipeline with PostgreSQL sink

--- spark/main.py
+++ spark/main.py
@@ -1,6 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json
 from schemas.telemetry_schema import telemetry_schema
+from pyspark.sql.functions import to_timestamp
+
+
+
 spark = (
     SparkSession.builder
     .appName("LogiRoute")
@@ -37,16 +41,61 @@ telemetry_df = (
     )
     .select("data.*")
 )
+
+db_df = (
+    telemetry_df
+    .select(
+        col("metadata.event_id").alias("event_id"),
+        to_timestamp(
+            col("metadata.timestamp")
+        ).alias("event_timestamp"),
+
+        col("payload.vehicle_id").alias("vehicle_id"),
+        col("payload.driver_id").alias("driver_id"),
+        col("payload.delivery_id").alias("delivery_id"),
+        col("payload.route_id").alias("route_id"),
+
+        col("payload.latitude").alias("latitude"),
+        col("payload.longitude").alias("longitude"),
+
+        col("payload.speed_kmph").alias("speed"),
+        col("payload.fuel_level_percent").alias("fuel_level"),
+        col("payload.vehicle_status").alias("status")
+    )
+)
+
 print("Schema loaded successfully")
 telemetry_df.printSchema()
+
+def write_to_postgres(batch_df, batch_id):
+
+    print(f"Writing batch {batch_id}")
+
+    (
+        batch_df.write
+        .format("jdbc")
+        .option("url", "jdbc:postgresql://postgres:5432/logiroute")
+        .option("dbtable", "vehicle_telemetry")
+        .option("user", "postgres")
+        .option("password", "postgres")
+        .option("driver", "org.postgresql.Driver")
+        .mode("append")
+        .save()
+    )
+
+
 query = (
-    telemetry_df
+    db_df
     .writeStream
-    .format("console")
-    .outputMode("append")
-    .option("truncate", False)
-    .option("checkpointLocation", "/opt/logiroute/checkpoints/telemetry")
+    .foreachBatch(write_to_postgres)
+    .option(
+        "checkpointLocation",
+        "/opt/logiroute/checkpoints/postgres"
+    )
     .start()
 )
-print("Streaming started...")
+
+print("Streaming to PostgreSQL...")
+
 query.awaitTermination()
+
